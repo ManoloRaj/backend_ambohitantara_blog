cafa82ed50cbfb16e4abdbe6c567825df086b852
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2023-06-24 17:06:37 +0200

    feat(app): first upload of the code

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue dans mon projet Express.js !');
+});
+
+app.listen(4000, () => {
+  console.log('Le serveur est en cours d\'exécution sur le port 3000.');
+});

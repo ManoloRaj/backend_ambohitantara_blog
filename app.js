a1edaea79dc6f733911424086001278d0fbada7d
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2023-06-24 17:52:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Bienvenue dans mon projet Express.js !');
 });
 
-app.listen(3000, () => {
+app.listen(() => {
   console.log('Le serveur est en cours d\'exécution sur le port 3000.');
 });
